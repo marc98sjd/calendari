fb040e8e313fc15b7316c9a5e7d9da5b15d1fcf1
marc98sjd <32141427+marc98sjd@users.noreply.github.com>
2017-10-06 18:44:08 +0200

    arreglat una indentació que no estaba be

--- calendari.php
+++ calendari.php
@@ -14,49 +14,49 @@
 	<head>
 		<title>Calendari mguerra</title>
 		<meta charset="utf-8">
-			<style>
-		#calendar .hoy {
-			background-color:red;
-		}
-	</style>
+		<style>
+			#calendar .hoy{
+				background-color:red;
+			}
+		</style>
 	</head>
 	<body>
 		<div>
 			<h1>Calendari en php</h1>
-		<table id="calendar">
-			<caption><?php echo $mesos[$mes]." ".$any?></caption> <!-- aqui es mostrara el mes i l'any-->	
-			<tr>
-				<th>Dilluns</th><th>Dimarts</th><th>Dimecres</th><th>Dijous</th>
-				<th>Divendres</th><th>Dissabte</th><th>Diumenge</th>
-			</tr>
-			<tr>
-				<?php
-					$last_cell=$diaSemana+$ultimDiaMes;
-					// fem un bucle fins 42, que es el maxim de cel·les que hi pot haber... 6 columnes de 7 dies
-					for($i=1;$i<=42;$i++){
-						if($i==$diaSemana){
-							// determinem en quin dia comença el mes
-							$dia=1;
+			<table id="calendar">
+				<caption><?php echo $mesos[$mes]." ".$any?></caption> <!-- aqui es mostrara el mes i l'any-->	
+				<tr>
+					<th>Dilluns</th><th>Dimarts</th><th>Dimecres</th><th>Dijous</th>
+					<th>Divendres</th><th>Dissabte</th><th>Diumenge</th>
+				</tr>
+				<tr>
+					<?php
+						$last_cell=$diaSemana+$ultimDiaMes;
+						// fem un bucle fins 42, que es el maxim de cel·les que hi pot haber... 6 columnes de 7 dies
+						for($i=1;$i<=42;$i++){
+							if($i==$diaSemana){
+								// determinem en quin dia comença el mes
+								$dia=1;
+							}
+							if($i<$diaSemana || $i>=$last_cell){
+								// cel·la buida
+								echo "<td>&nbsp;</td>";
+							}
+							else{
+								// mostrem el dia
+								if($dia==$diaActual)
+									echo "<td class='hoy'>$dia</td>";
+								else
+									echo "<td>$dia</td>";
+								$dia++;
+							}
+							//al final de la setmana es crea una nova fila
+							if($i%7==0){
+								echo "</tr><tr>\n";
+							}
 						}
-						if($i<$diaSemana || $i>=$last_cell){
-							// cel·la buida
-							echo "<td>&nbsp;</td>";
-						}
-						else{
-							// mostrem el dia
-							if($dia==$diaActual)
-								echo "<td class='hoy'>$dia</td>";
-							else
-								echo "<td>$dia</td>";
-							$dia++;
-						}
-						//al final de la setmana es crea una nova fila
-						if($i%7==0){
-							echo "</tr><tr>\n";
-						}
-					}
-				?>
-			</tr>
+					?>
+				</tr>
 			</table>
 		</div>
 	</body>
